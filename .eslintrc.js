5c506698dfcee950ebfbbeb24c2c860e874bfb83
赵仁建 <zhaorenjian@bytedance.com>
2021-09-02 11:42:43 +0800

    feat: 完善eslint prettier配置

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    "node": true
+  },
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "prettier"
+  ],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  rules: {
+  }
+}
